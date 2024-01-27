ad612201ba4505de318de2966f9fa9b55385fc7e
Gracjan Puch <gracjan@ftgt.org>
2024-01-27 20:33:44 +0100

    Change obstacles and helpers prefabs graphics

--- Assets/Scripts/LevelModificator/DynamicallyLoadedLevelElement.cs
+++ Assets/Scripts/LevelModificator/DynamicallyLoadedLevelElement.cs
@@ -3,6 +3,8 @@ public class DynamicallyLoadedLevelElement : MonoBehaviour
 {
     public enum MapElementType {Obstacle, Helper}
 
+    public bool allowRandomGeneration=true;
+
     public MapElementType mapElementType;
     public string key;
     public string userName;
