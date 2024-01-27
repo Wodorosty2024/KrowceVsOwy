4a2636c1298e200d58cff8a8e5f34ce5771c1ee7
Gracjan Puch <gracjan@ftgt.org>
2024-01-27 14:47:36 +0100

    Add API level fetch, UI scripts and character jumping

--- Assets/Scripts/LevelModificator/DynamicallyLoadedLevelElement.cs
+++ Assets/Scripts/LevelModificator/DynamicallyLoadedLevelElement.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 public class DynamicallyLoadedLevelElement : MonoBehaviour
 {
+    public enum MapElementType {Obstacle, Helper}
+
+    public MapElementType mapElementType;
     public string key;
+    public string userName;
+    public string userComment;
 }
